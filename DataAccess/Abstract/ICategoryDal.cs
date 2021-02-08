1d6fba924331ffb09e25981758ed3a6d324ee836
Fahrican Kaçan <fahrican.kcn@gmail.com>
2021-02-08 19:37:33 +0300

    generic reposityory design pattern

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> GetAll();
+        void Add(Category product);
+        void Update(Category product);
+        void Delete(Category product);
+
+        List<Category> GetAllByCategory(int categoryId);
+    }
+}
+//GENERİC REPOSİTORY DESİGN PATTER 
