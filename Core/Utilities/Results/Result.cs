d677b46267226e1528abe32244a45c797d595a2f
Fahrican Kaçan <fahrican.kcn@gmail.com>
2021-02-10 22:29:06 +0300

    Core katmanına Results eklendi.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool succes,string message)
+        {
+            this.Succes = Succes;
+            this.Message = Message;
+        }
+
+        public bool Succes { get; }
+
+        public string Message { get; }
+    }
+}
